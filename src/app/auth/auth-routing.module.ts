231d73b4e5cd1208824d10d15792e03ab39b1135
issamjebrane <issamjebrane65@gmail.com>
2024-07-14 19:10:11 +0100

    minor changes

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -13,10 +13,10 @@ const routes: Routes = [
         path:'login',
         component:LoginComponent,
       },
-      {
-        path:'register',
-        component:RegisterComponent
-      }
+      // {
+      //   path:'register',
+      //   component:RegisterComponent
+      // }
     ]
   }
 ];
