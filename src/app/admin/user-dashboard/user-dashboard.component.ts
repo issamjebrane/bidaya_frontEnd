d8bf918ac3bec5bfb10af3b290a0dfae10fac939
issamjebrane <issamjebrane65@gmail.com>
2024-07-26 11:56:47 +0100

    admin dashboard implementation

--- src/app/admin/user-dashboard/user-dashboard.component.ts
+++ src/app/admin/user-dashboard/user-dashboard.component.ts
@@ -1,27 +1,43 @@
 import {Component, OnInit} from '@angular/core';
 import {User} from "../../../types/user.types";
 import {UsersService} from "../../services/users.service";
+import {DatePipe} from "@angular/common";
 
 @Component({
   selector: 'app-user-dashboard',
   templateUrl: './user-dashboard.component.html',
-  styleUrl: './user-dashboard.component.sass'
+  styleUrl: './user-dashboard.component.sass',
+  providers: [DatePipe]
 })
 export class UserDashboardComponent implements OnInit{
 
   users:User[] = []  as User[];
-
-  constructor(private userService: UsersService) {
+  date:Date = new Date();
+  currentDate: string | null = '';
+  isLoading: boolean = true;
+  isDeleted: boolean = false;
+  constructor(private userService: UsersService,private datePipe: DatePipe) {
   }
 
   ngOnInit(): void {
     this.userService.getUsers().subscribe({
       next: users => {
         this.users = users
-        console.log(users)
+        this.isLoading = false;
       },
       error: err => console.error(err)
     });
+    this.currentDate = this.datePipe.transform(this.date,'yyyy-MM-dd');
   }
 
+  delete(id:number | undefined) {
+    if(id !== undefined)
+    this.userService.deleteUser(id).subscribe({
+        next:() =>{
+          this.isDeleted = true;
+        },
+        error: (err: Error) => console.error(err)
+      },
+      )
+  }
 }
--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {User} from "../../types/user.types";
 import {environment} from "../../environments/environment";
+import {Observable} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
@@ -12,5 +13,10 @@ export class UsersService {
 
   getUsers() {
     return this.http.get<User[]>(`${environment.API}/users/all-users`);
+  }
+
+  deleteUser(id:number):Observable<any> {
+    //send user id to be deleted as a query parameter
+    return this.http.delete(`${environment.API}/users/delete-user`,{params:{id:id}});
   }
 }
